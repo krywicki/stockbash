076af6623084d6e1999e480330db8ed23d786f24
Joe Krywicki <jkrywicki@yourdestinyapp.com>
2020-09-25 00:32:52 -0500

    added global quote endpoint; refactored
    ### Global Quote Endpoint ###
    - The endpoint is for fetching simplified time series data.
    
    ### Refactored ###
    - Small refactoring done.
    
    * Removed jq command requirement, will use csv data
    * Removed retval and now use $(<fn>) for direct returns
    * Added stub code for displaying single stock

--- stockbash.sh
+++ stockbash.sh
@@ -4,46 +4,9 @@ VERSION="0.0.1"
 APIKEY=${STOCKBASH_APIKEY:-missing}
 URL="https://www.alphavantage.co"
 
-# Performs HTTP GET 
-# Usage: http_get [path] [query param]
-#   Example: http_get 'https://google.com' 'param1=value1' 'param2=value2'
-function http_get() {
-    local url=$1
-    local cmd="curl -G -d 'apikey=$APIKEY'"
-
-    shift
-    for qparam in $@
-    do
-        local cmd="$cmd -d '$qparam'"
-    done
-
-    # append url path
-    local cmd="$cmd $url"
-
-    eval $cmd
-}
-
-# Get global quote for specified symbol
-# Usage: global_quote [symbol]
-#   Example: global_quote IBM
-function global_quote() { 
-    http_get "$URL/query" "function=GLOBAL_QUOTE" "symbol=$1"
-}
-
-# Get as-traded daily time series (date, daily open, daily high, daily low, daily close, daily volume) for
-# specified symbol
-# Usage: time_series_daily [symbol]
-#   Example: time_series_daily AAPL
-function time_series_daily() {
-    http_get "$URL/query" "function=TIME_SERIES_DAILY" "symbol=$1"
-}
-
-# Get company info, financial raitios and other key mterics for equity specified. 
-# Usage: company_overiew [symbol]
-#   Example: company_overiew IBM 
-function company_overview {
-    http_get "$URL/query" "function=OVERVIEW" "symbol=$1"
-}
+##############################################################################
+# Helper Functions
+##############################################################################
 
 # Checks if program is present in environment
 # MISSING_DEPENDENCIES=True will be set if program is not found.
@@ -61,11 +24,95 @@ function check_for_program() {
 # Checks for required dependencies. Exits if dependency missing.
 # Usage: check_for_dependendencies
 function check_for_dependencies() {
-    check_for_program jq
-
     if [ ! -z $MISSING_DEPENDENCIES ]; then
         exit 1
     fi
+}
+
+##############################################################################
+# Alphavantage API Functions
+##############################################################################
+
+# Performs HTTP GET 
+# Usage: http_get [path] [query params]
+#   Example: http_get 'https://google.com' 'param1=value1' 'param2=value2'
+function http_get() {
+    local url=$1
+    local cmd="curl -Gs -d 'apikey=$APIKEY'"
+
+    shift
+    for qparam in $@
+    do
+        local cmd="$cmd -d '$qparam'"
+    done
+
+    # append url path
+    local cmd="$cmd $url"
+
+    echo $(eval "$cmd")
+}
+
+# Get global quote for specified symbol
+# Usage: global_quote [symbol]
+#   Example: global_quote IBM
+function get_global_quote() { 
+    echo $(http_get "$URL/query" "function=GLOBAL_QUOTE" "symbol=$1")
+}
+
+# Get as-traded daily time series (date, daily open, daily high, daily low, daily close, daily volume) for
+# specified symbol
+# Usage: time_series_daily [symbol]
+#   Example: time_series_daily AAPL
+function get_time_series_daily() {
+    echo $(http_get "$URL/query" "function=TIME_SERIES_DAILY" "symbol=$1")
+}
+
+# Get company info, financial raitios and other key mterics for equity specified. 
+# Usage: company_overiew [symbol]
+#   Example: company_overiew IBM 
+function get_company_overview {
+    echo $(http_get "$URL/query" "function=OVERVIEW" "symbol=$1")
+}
+
+# Get global quote for target symbol
+# Usage: get_global_quote [symbol]
+#   Example: get_global_quote IBM
+function get_global_quote() {
+    local csv=$(http_get "$URL/query" "function=GLOBAL_QUOTE" "datatype=csv" "symbol=$1")
+    read -r -a array <<< $csv
+    IFS=',' read -a data <<< ${array[1]}
+    echo ${data[@]}
+}
+
+
+##############################################################################
+# Functions
+##############################################################################
+
+# Presents stock summary detailing current stock price, 
+# Usage: display_stock_summary [symbol]
+#   Example: dispaly_stock_summary IBM
+function display_stock_summary() {
+    local csv=($(get_global_quote $1))
+    local open=${csv[1]}
+    local high=${csv[2]}
+    local low=${csv[3]}
+    local price=${csv[4]}
+    local volume=${csv[5]}
+    local latest=${csv[6]}
+    local prev_close=${csv[7]}
+    local change=${csv[8]}
+    local change_percent=${csv[9]}
+
+    printf -- " _____________________\n"
+    printf    "|\e[4m %-20s\e[0m|\n" $1              
+    printf    "| Open %-10s   |\n"  $open 
+    printf    "|                     |\n"
+    printf    "|                     |\n"
+    printf    "|                     |\n"
+    printf    "|                     |\n"
+    printf    "|                     |\n"
+    printf -- "|_____________________|\n"
 }
 
 ########################################################
@@ -73,6 +120,13 @@ function check_for_dependencies() {
 ########################################################
 
 check_for_dependencies
-company_overview IBM
+
+display_stock_summary AAPL
+
+#display_stock_summary AB
+#display_stock_summary ABC
+#display_stock_summary ABCD
+#display_stock_summary ABCDE
+#display_stock_summary ABCDEF
 
 
